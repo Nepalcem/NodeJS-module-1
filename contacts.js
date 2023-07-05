6e95e5195aab1c959882cb35492b3d737dff2677
Nepalcem <mark.emailtest@gmail.com>
2023-07-05 22:51:30 +0300

    removeContact

--- contacts.js
+++ contacts.js
@@ -12,17 +12,32 @@ const listContacts = async () => {
 };
 
 const getContactById = async (contactId) => {
+  try {
+    const contactsArray = await listContacts();
+    const element = contactsArray.find((el) => el.id === contactId);
+    if (!element) {
+      return null;
+    }
+    console.log(element);
+    return element;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactId) => {
   const contactsArray = await listContacts();
   const element = contactsArray.find((el) => el.id === contactId);
   if (!element) {
     return null;
   }
-  console.log(element);
-  return element;
+  // console.log(element);
+  const indexOfElement = contactsArray.indexOf(element);
+  const deletedElement = contactsArray.splice(indexOfElement, 1);
+
+  const contactsPath = path.join("db", "contacts.json");
+  fs.writeFile(contactsPath, JSON.stringify(contactsArray), "utf8");
+  return deletedElement;
 };
 
-const removeContact = async (contactId) => {
-  const contactsArray = await listContacts();
-
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-};
+removeContact("AeHIrLTr6JkxGE6SN-0Rw");
